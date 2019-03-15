b8c56ab8a165b9782ef432c7a2b224dc0f204b5a
tsukuba3 <tellex@poppy.ocn.ne.jp>
2019-03-15 14:21:47 +0900

    計算の正確性が確認されました。 足し算だけですが、きちんと動きます。 各関数のユニットテストを書いて、正しく評価し、不要な部分をそぎ落とす作業をする予定です。

--- calc.c
+++ calc.c
@@ -2,12 +2,110 @@
 #include <stdlib.h>
 #include <string.h>
 
-int OperandCatch(char *exp, int *exp_cnt);
+int wordSeparater(char *exp, int *exp_cnt);
+int decodeData( char *s, int len );
 
 char tmp[128];
 
-/* デコード処理*/
-int decode_data( char *s, int len )
+int main(void)
+{
+	printf("Content-type:text/html\n\n");
+	char query_string[128];
+	strcpy(query_string, getenv("QUERY_STRING"));	
+	decodeData(query_string, strlen(query_string));
+
+	int i,j;
+
+	for(int k=12;k<strlen(query_string);k++)
+	{
+		tmp[i++] = query_string[k];
+	}
+
+	printf("%s\n", tmp);
+
+	int ans = 0;
+	int operand[10];
+	int operator[9];
+
+	int operand_cnt;
+
+	i = j = 0;
+	operand_cnt = 0;
+
+	int len = strlen(tmp);
+
+	printf("G=%s;", tmp);
+	printf("H=;");
+
+	while(i < len)
+	{
+		printf("%c", tmp[i]);
+
+		if('+' == tmp[i])
+		{
+			operator[j++] = tmp[i];
+			i++;
+		}
+		else if('=' == tmp[i])
+		{
+			break;
+		}
+		else
+		{
+			operand[operand_cnt++] = wordSeparater(tmp, &i);
+		}
+	}
+
+	j = 0;
+	int subtotal = operand[0];
+
+	// 加算回路未完成(式のパースまでは完了)
+	for(i=1;i<operand_cnt;i++)
+	{
+		if('+' == operator[j])
+		{
+			subtotal += operand[i];
+			j++;
+		}
+	}
+
+	printf("%d\n", subtotal);
+
+	return 0;
+}
+
+/* オペランドを取り出して整数に変換 */
+int wordSeparater(char *exp, int *exp_cnt)
+{
+	int save = *exp_cnt;
+	int i,k;
+	char tmp_operand[5];
+	int ans = 0;
+
+	i = *exp_cnt;
+	k = 0;
+
+	while('+' != exp[i])
+	{
+		if('=' == exp[i])
+			break;
+
+		tmp_operand[k] = exp[i];
+
+		i++; k++;
+	}
+
+	tmp_operand[k] = '\0';
+
+	ans += atoi(tmp_operand);
+
+	*exp_cnt = save + k;
+
+	return ans;
+}
+
+/* URLデコード処理*/
+int decodeData( char *s, int len )
 {
    /* len はs が保持するデータのサイズ*/
    int i, j, k;     /* カウンタ類*/
@@ -42,94 +140,3 @@ int decode_data( char *s, int len )
    free( s1 );
    return 0;
 }
-
-void getElement(char *str)
-{
-	int i=0;
-	int j=0;
-
-	while('=' != str[i])
-	{
-		i++;		
-	}
-	for(i=i+1;i<128;i++)
-	{
-		tmp[j++] = str[i];
-	}
-}
-
-int main(void)
-{
-	printf("Content-type:text/html\n\n");
-	char query_string[128];
-	strcpy(query_string, getenv("QUERY_STRING"));
-	getElement(query_string);
-	
-	decode_data(tmp, strlen(tmp));
-	printf("%s", tmp);
-
-	int ans = 0;
-	int operand[10];
-	int operator[9];
-
-	int i,j;
-	int operand_cnt;
-
-	i = j = 0;
-	operand_cnt = 0;
-
-	int len = strlen(tmp);
-
-	while(i < len)
-	{
-		if('+' == tmp[i])
-		{
-			i += 1;
-		}
-		else
-		{
-			operand[operand_cnt] = OperandCatch(tmp, &i);
-			operand_cnt++;
-		}
-
-		i++;
-	}
-
-	for(i=0;i<operand_cnt;i++)
-	{
-		ans += operand[i];
-	}
-
-	printf("%d\n", ans);
-
-	return 0;
-}
-
-int OperandCatch(char *exp, int *exp_cnt)
-{
-	int i,k;
-	char tmp_operand[5];
-	int ans = 0;
-
-	i = *exp_cnt;
-	k = 0;
-
-	while('+' != exp[i])
-	{
-		if('=' == exp[i])
-			break;
-
-		tmp_operand[k] = exp[i];
-
-		i++; k++;
-	}
-
-	tmp_operand[k] = '\0';
-
-	ans += atoi(tmp_operand);
-
-	*exp_cnt = i;
-
-	return ans;
-}
-
